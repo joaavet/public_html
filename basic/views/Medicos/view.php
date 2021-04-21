215b5111b0eef0781c93f7447553af666789fb05
joaavet <jv397803@gmail.com>
2021-04-21 11:49:20 -0300

    first commit

---
+++ basic/views/Medicos/view.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1>medico/view</h1>
+
+<p>
+    You may change the content of this page by modifying
+    the file <code><?= __FILE__; ?></code>.
+</p>
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
